6ab31c0ed3355963844f89da97431674bbc6a60a
CindoddCindy <cindodcindy@gmail.com>
2021-03-05 14:34:50 +0700

    tambah package

---
+++ otmBiderectional/src/main/java/com/otmbidirectional/otmBiderectional/OtmBiderectionalApplication.java
@@ -0,0 +1,13 @@
+package com.otmbidirectional.otmBiderectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmBiderectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmBiderectionalApplication.class, args);
+	}
+
+}
---
+++ otmBiderectional/src/test/java/com/otmbidirectional/otmBiderectional/OtmBiderectionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otmbidirectional.otmBiderectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmBiderectionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
